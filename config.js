10c4917f4af6d7d3e9089893b00286acf717cf40
Satria356 <102003881+Satria356@users.noreply.github.com>
2022-04-16 07:38:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,17 +17,17 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285162634171','6285748894171']
+global.owner = ['6283811034750','6283811034750']
 global.web = 'https://youtube.com/channel/UC2hMC3YSXSrDDhqOECXZkUg'
-global.bot = 'NahBotz-MD²'
+global.bot = 'MLBB-MD'
 global.premium = ['6285748894171']
-global.packname = 'NakataArdnt'
-global.author = 'Nakata'
+global.packname = 'MLBB-BOT'
+global.author = 'Ardnt'
 global.sessionName = 'nahh'
 global.prefa = ['','!','.','🐦','🐤','🗿']
-global.sp = '⭔'
+global.sp = '≥'
 global.mess = {
-    success: '✓ Success',
+    success: ' Success',
     admin: 'Fitur Khusus Admin Group!',
     botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
     owner: 'Fitur Khusus Owner Bot',
